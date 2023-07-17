6abf735366f6238cef53151a4d00443a1974bc8a
budisentosa <budi@avasoft.co>
2023-07-17 09:20:38 +0700

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+const username = useState('username')
+</script>
+
+<template>
+  <div>
+    <h1>Home</h1>
+    <p>In order to enter the secret page, you need to fill your username:</p>
+    <p><input v-model="username" placeholder="Username" /></p>
+    <NuxtLink to="/secret">Secret page</NuxtLink>
+  </div>
+</template>
---
+++ pages/secret.vue
@@ -0,0 +1,13 @@
+<script setup>
+definePageMeta({
+  middleware: 'auth',
+});
+const username = useState('username');
+</script>
+
+<template>
+  <div>
+    <h1>Welcome {{ username }}</h1>
+    <NuxtLink to="/">Home</NuxtLink>
+  </div>
+</template>
